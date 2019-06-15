c90970198f246c1eb448d2195894e964640b9b6d
= <133541014@qq.com>
2019-06-16 00:33:02 +0800

    配置docker

--- src/test/java/test/RedisTest.java
+++ src/test/java/test/RedisTest.java
@@ -1,6 +1,7 @@
 
 package test;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import com.origen.faceshare.model.entity.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
+@Ignore
 public class RedisTest {
 
 	@Autowired
